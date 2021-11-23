2fafacaf98484e0877025f3fd862243eee07c136
khangvt146 <khangvotan@gmail.com>
2021-11-23 23:57:42 +0700

    add khoa hoc

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var courseapi = require("../public/api/course.json");
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('khoahoc', {
+        title: 'Online Course Management',
+        CourseArray: courseapi,
+    });
 });
 
 module.exports = router;
