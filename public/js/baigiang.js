aa166b64cb5175be3647b27f714c4b8001e0282a
khangvt146 <khangvotan@gmail.com>
2021-11-29 10:05:52 +0700

    updatede

--- public/js/baigiang.js
+++ public/js/baigiang.js
@@ -1,62 +1,63 @@
-/* Show baigiang detail js */
-function SetShowDetailBaigiang() {
-    var baigiangSelected = document.querySelectorAll(".baigiang_container #i_btn");
-    baigiangSelected.forEach((baigiang) => {
-        baigiang.onclick = function(e) {
-            var tem = document.querySelector(".info-baigiang-container");
-            tem.classList.add("info__active");
-            var info = baigiang.innerText.split("\n");
-        };
-        return baigiang;
-    });
-}
+// /* Show baigiang detail js */
+// function SetShowDetailBaigiang() {
+//     var baigiangSelected = document.querySelectorAll(".baigiang_container #i_btn");
+//     baigiangSelected.forEach((baigiang) => {
+//         baigiang.onclick = function(e) {
+//             var tem = document.querySelector(".info-baigiang-container");
+//             tem.classList.add("info-baigiang__active");
+//             var info = baigiang.innerText.split("\n");
+//         };
+//         return baigiang;
+//     });
+//     info
+// }
 
-var outBaigiangInfo = document.querySelector(".close__info-icon");
-outBaigiangInfo.onclick = function(e) {
-    var tem = document.querySelector(".info-baigiang-container");
-    tem.classList.remove("info__active");
-};
+// var outBaigiangInfo = document.querySelector(".close__info-icon");
+// outBaigiangInfo.onclick = function(e) {
+//     var tem = document.querySelector(".info-baigiang-container");
+//     tem.classList.remove("info-baigiang__active");
+// };
 
-/* Add baigiang js */
-function SetAddbaigiang() {
-    var baigiangAdd = document.querySelectorAll(".dropdown_baigiang .add_button");
-    baigiangAdd.forEach((baigiang) => {
-        baigiang.onclick = function(e) {
-            var tem = document.querySelector(".add-baigiang-container");
-            tem.classList.add("add-baigiang--active");
-        };
-        return baigiang;
-    });
-}
+// /* Add baigiang js */
+// function SetAddbaigiang() {
+//     var baigiangAdd = document.querySelectorAll(".dropdown_baigiang .add_button");
+//     baigiangAdd.forEach((baigiang) => {
+//         baigiang.onclick = function(e) {
+//             var tem = document.querySelector(".add-baigiang-container");
+//             tem.classList.add("add-baigiang--active");
+//         };
+//         return baigiang;
+//     });
+// }
 
-var outAddBaigiang = document.querySelector(".close__add-baigiang-icon");
-outAddBaigiang.onclick = function(e) {
-    var tem = document.querySelector(".add-baigiang-container");
-    tem.classList.remove("add-baigiang--active");
-};
-
-
-/* Update baigiang js */
-function SetUpdatebaigiang() {
-    var baigiangUpdate = document.querySelectorAll(".baigiang_container #u_btn");
-    baigiangUpdate.forEach((baigiang) => {
-        baigiang.onclick = function(e) {
-            var tem = document.querySelector(".update-baigiang-container");
-            tem.classList.add("update-baigiang--active");
-        };
-        return baigiang;
-    });
-}
-
-var outUpdateBaigiang = document.querySelector(".close__update-baigiang-icon");
-outUpdateBaigiang.onclick = function(e) {
-    var tem = document.querySelector(".update-baigiang-container");
-    tem.classList.remove("update-baigiang--active");
-};
+// var outAddBaigiang = document.querySelector(".close__add-baigiang-icon");
+// outAddBaigiang.onclick = function(e) {
+//     var tem = document.querySelector(".add-baigiang-container");
+//     tem.classList.remove("add-baigiang--active");
+// };
 
 
+// /* Update baigiang js */
+// function SetUpdatebaigiang() {
+//     var baigiangUpdate = document.querySelectorAll(".baigiang_container #u_btn");
+//     baigiangUpdate.forEach((baigiang) => {
+//         baigiang.onclick = function(e) {
+//             var tem = document.querySelector(".update-baigiang-container");
+//             tem.classList.add("update-baigiang--active");
+//         };
+//         return baigiang;
+//     });
+// }
 
-/* Call function */
-SetShowDetailBaigiang();
-SetAddbaigiang();
-SetUpdatebaigiang();
+// var outUpdateBaigiang = document.querySelector(".close__update-baigiang-icon");
+// outUpdateBaigiang.onclick = function(e) {
+//     var tem = document.querySelector(".update-baigiang-container");
+//     tem.classList.remove("update-baigiang--active");
+// };
+
+
+
+// /* Call function */
+// SetShowDetailBaigiang();
+// SetAddbaigiang();
+// SetUpdatebaigiang();
