e3ccd346799e2e2f478dae610eccfe89f9901a4d
hiepnguyen3872 <59463682+hiepnguyen3872@users.noreply.github.com>
2021-11-30 10:36:35 +0700

    update course

--- models/course.js
+++ models/course.js
@@ -8,7 +8,7 @@ let connection = mysql.createConnection({
 });
 
 exports.getAllCourse = async function() {
-    return await connection.awaitQuery(`SELECT khoa_hoc.ma_trinh_do, khoa_hoc.ma_kh, khoa_hoc.ten_kh, khoa_hoc.ma_combo, khoa_hoc.ma_mon_hoc, khoa_hoc.ngay_bat_dau, khoa_hoc.ngay_ket_thuc, khoa_hoc.han_dk, khoa_hoc.hoc_phi, khoa_hoc.mo_ta, khoa_hoc.kieu_kh, khoa_hoc.hinh_anh, trinh_do.ten, trinh_do.loai_trinh_do, CONCAT(nguoi_dung.ho,  ' ', nguoi_dung.ten_lot, ' ', nguoi_dung.ten) AS ten_gv FROM khoa_hoc, trinh_do, day_hoc, nguoi_dung WHERE khoa_hoc.ma_trinh_do = trinh_do.ma_trinh_do AND day_hoc.ma_kh = khoa_hoc.ma_kh AND nguoi_dung.id = day_hoc.ma_gv ORDER BY ma_kh ASC;`);
+    return await connection.awaitQuery(`SELECT khoa_hoc.ma_kh, khoa_hoc.ma_trinh_do, khoa_hoc.ten_kh, khoa_hoc.ma_combo, khoa_hoc.ma_mon_hoc, khoa_hoc.ngay_bat_dau, khoa_hoc.ngay_ket_thuc, khoa_hoc.han_dk, khoa_hoc.hoc_phi, khoa_hoc.mo_ta, khoa_hoc.kieu_kh, khoa_hoc.hinh_anh, trinh_do.ten, trinh_do.loai_trinh_do, CONCAT(nguoi_dung.ho,  ' ', nguoi_dung.ten_lot, ' ', nguoi_dung.ten) AS ten_gv FROM khoa_hoc, trinh_do, day_hoc, nguoi_dung WHERE khoa_hoc.ma_trinh_do = trinh_do.ma_trinh_do AND day_hoc.ma_kh = khoa_hoc.ma_kh AND nguoi_dung.id = day_hoc.ma_gv GROUP BY ma_kh ORDER BY ma_kh ASC;`);
 };
 
 exports.getLevelID = async function(course_class, course_level) {
